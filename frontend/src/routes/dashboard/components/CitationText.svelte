aa289007e7003803978a6695068030e37624b087
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-07-20 10:41:14 -0700

    remove data.ts imports

--- frontend/src/routes/dashboard/components/CitationText.svelte
+++ frontend/src/routes/dashboard/components/CitationText.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import OpenAI from 'openai';
-	import { data } from '../../../lib/data';
 
 	const openai = new OpenAI({
 		apiKey: import.meta.env.VITE_OPENAI_APIKEY,
