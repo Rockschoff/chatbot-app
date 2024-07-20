e09c366589ff530f118e67c66befef6b150175c0
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-07-20 10:38:40 -0700

    add API based file name retreival

--- frontend/src/routes/dashboard/[file_id]/+page.server.js
+++ frontend/src/routes/dashboard/[file_id]/+page.server.js
@@ -1,14 +1,26 @@
 import {error} from "@sveltejs/kit"
 import {data} from "../../../lib/data"
+import { OpenAI} from 'openai';
 
-export function load({params}){
+const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_APIKEY,
+    dangerouslyAllowBrowser: true
+});
+
+export async function load({params}){
+    let file;
     console.log(params)
-    const file = data.find((d)=>d.file_id===params.file_id)
+    try{
+        file =  await openai.files.retrieve(params.file_id); //data.find((d)=>d.file_id===params.file_id)
+    }catch(e){
+        console.log("Error getting filename from openai")
+        throw error(404)
+    }
     console.log("found the file" , file)
     if (!file) throw error(404)
     return {
-        file_name : file.file_name,
-        file_id : file.file_id
+        file_name : file.filename,
+        file_id : params.file_id
     }
 }
 
