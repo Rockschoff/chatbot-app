433e6c67c84f5045090df22a5cac9f27670676d4
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-07-21 09:31:57 -0700

    fix file rettreival

--- frontend/src/routes/dashboard/[file_id]/+page.server.js
+++ frontend/src/routes/dashboard/[file_id]/+page.server.js
@@ -1,10 +1,20 @@
 import {error} from "@sveltejs/kit"
-import {data} from "../../../lib/data"
+import {OpenAI} from 'openai'
+
+const openai = new OpenAI({
+    apiKey: import.meta.env.VITE_OPENAI_APIKEY,
+    dangerouslyAllowBrowser: true
+});
 
 export async function load({params}){
-    let file;
     console.log(params)
-    const file = data.find((d)=>d.file_id===params.file_id)
+    let file
+    try{
+        file = await openai.files.retrieve(params.file_id);
+    }catch(e){
+        console.log("Error Getting file from openai" , e)
+    }
+    
     console.log("found the file" , file)
     if (!file) throw error(404)
     return {
