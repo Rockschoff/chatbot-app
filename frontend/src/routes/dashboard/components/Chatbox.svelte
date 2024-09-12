943808098e95a5077b3b45c5f5296e2f83f8ea4b
Rishi Dubey <50651298+Rockschoff@users.noreply.github.com>
2024-09-12 10:10:00 -0700

    allow cfr search

--- frontend/src/routes/dashboard/components/Chatbox.svelte
+++ frontend/src/routes/dashboard/components/Chatbox.svelte
@@ -144,7 +144,7 @@
 			const stream = await openai.beta.threads.runs.create(threadId, {
 				assistant_id: import.meta.env.VITE_ASSISTANTID, 
 				stream: true,
-				tool_choice: {"type": "file_search"},
+				tool_choice: "auto",
 				additional_instructions:"You want to gather as much information as you can. It is mandatory for you to do both, search the CFR and Search the files that are uploaded"
 			});
 			
